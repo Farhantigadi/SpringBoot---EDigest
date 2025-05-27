1081c3822a16ca9988ffc3882d4faca4327720d9
Farhantigadi <farhantigadi123@gmail.com>
2025-05-27 13:35:21 +0530

    Learnt autowired and implemented basis of it

---
+++ src/main/java/Edigest/First/Springboot/Project/Engine.java
@@ -0,0 +1,11 @@
+package Edigest.First.Springboot.Project;
+
+import org.springframework.stereotype.Component;
+
+@Component
+class Engine {
+    public String func(){
+        return "Engine started!";
+    }
+}
+
--- src/main/java/Edigest/First/Springboot/Project/MyClass.java
+++ src/main/java/Edigest/First/Springboot/Project/MyClass.java
@@ -1,12 +1,22 @@
 package Edigest.First.Springboot.Project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyClass {
     @GetMapping("hello")
-    public String sayhello(){
+    public String sayhello() {
         return "Hello";
     }
+
+    @Autowired
+    Engine engine;
+
+    @GetMapping("hello/eng")
+    public String eng() {
+        return engine.func();
+    }
+
 }
